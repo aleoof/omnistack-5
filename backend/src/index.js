631cbcf9d205d24dc82778e48c98d1e5919a39a1
Alexandre <alexandre_osvaldo@hotmail.com>
2019-11-01 17:11:05 -0300

    first commit

---
+++ backend/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const route = require('./routes');
+
+mongoose.connect(
+  'mongodb+srv://test-crud:omni123456@omnistack-rxmpz.gcp.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(route);
+
+server.listen(3333, () => {
+  console.log('server started on port 3000');
+});
